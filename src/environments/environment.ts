27ae0097a03bcf0bb5ef8bcb1f9caaf04fa9a816
kraenbyskov <kraenbyskov@gmail.com>
2020-03-04 09:23:56 +0000

    Firebase works and is uploaded

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDiE6ipW58lbNEHxGHruZ8r308vk87IydQ",
+    authDomain: "denmarkproyect.firebaseapp.com",
+    databaseURL: "https://denmarkproyect.firebaseio.com",
+    projectId: "denmarkproyect",
+    storageBucket: "denmarkproyect.appspot.com",
+    messagingSenderId: "569035060289",
+    appId: "1:569035060289:web:b02eac30bb0e87bbea82c0",
+    measurementId: "G-W004T8QHLF"
+  }
 };
 
 /*
