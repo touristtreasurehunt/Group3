7754de14d41186727ccdfbbcc3847c8be7eccdad
kraenbyskov <kraenbyskov@gmail.com>
2020-03-09 08:03:33 +0000

    Not working at all

--- src/app/pages/geolocation/geolocation.page.ts
+++ src/app/pages/geolocation/geolocation.page.ts
@@ -1,33 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import * as L from 'leaflet';
+import { Component, OnInit } from "@angular/core";
+import * as L from "leaflet";
+import { Map, tileLayer, marker, LatLng } from "leaflet";
 
 @Component({
-  selector: 'app-geolocation',
-  templateUrl: './geolocation.page.html',
-  styleUrls: ['./geolocation.page.scss'],
+  selector: "app-geolocation",
+  templateUrl: "./geolocation.page.html",
+  styleUrls: ["./geolocation.page.scss"]
 })
 export class GeolocationPage implements OnInit {
+  map: any;
 
-  map : any;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
+
+  locatePosition() {
+    this.map.locate({ setView: true }).on("locationfound", (e: any) => {
+      let newMarker = marker([e.latitude, e.longitude], {
+        draggable: false
+      }).addTo(this.map);
+      newMarker.bindPopup("You are located here!").openPopup();
+    });
+  }
 
   ionViewDidEnter() {
-    this.map = L.map('mapLeaflet', {
+    this.map = L.map("mapLeaflet", {
       center: [28.1281521, -15.4489039],
       zoom: 15,
       zoomControl: true
-  });
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
-  
-
-  L.marker([ 28.127567,-15.4469402], {draggable: false}).addTo(this.map);
-
+    });
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
+      this.map
+    );
+    console.log(L.LatLng);
+    L.marker([28.127567, -15.4469402], { draggable: false }).addTo(this.map);
   }
-
-  
-
-
-  
-
 }
