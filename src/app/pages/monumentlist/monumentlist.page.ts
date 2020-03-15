3faabb9a708e72aff314c1a19162e0b31865c9c9
eduvega99 <eduardovr1999@gmail.com>
2020-03-15 19:29:43 +0000

    Commit con la obtención de datos más clara y pequeño cambio en el apartado de pasar parámetros

--- src/app/pages/monumentlist/monumentlist.page.ts
+++ src/app/pages/monumentlist/monumentlist.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
+import { ConnectionService } from 'src/app/services/connection.service';
 
 @Component({
   selector: 'app-monumentlist',
@@ -10,10 +11,23 @@ export class MonumentlistPage implements OnInit {
 
   private place: string; 
 
-  constructor(private api: ApiService) { }
+  arrayBuildings: any = [];
+
+  constructor(private api: ApiService, private connectionService: ConnectionService) { }
 
   ngOnInit() {
-    this.api.$getObjectSource.subscribe(data => console.log(data));
+    this.getBuildings();
+  }
+
+  getBuildings() {
+    this.connectionService.getCollection("places").subscribe((query) => {
+      this.arrayBuildings = [];
+      query.forEach((datasPlaces: any) => {
+        if(datasPlaces.payload.doc.id == this.api.place) {
+          this.arrayBuildings = datasPlaces.payload.doc.data().buildings;
+        }
+      })      
+    });
   }
 
 }
